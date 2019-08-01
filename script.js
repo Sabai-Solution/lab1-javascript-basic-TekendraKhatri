4091525fb1192f2ca5203bbcf3f83282a68fbbf5
tekendra <tekendrakhatri44@gmail.com>
2019-08-01 08:43:40 +0545

    Making empty validation

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function validateEmptyValues(){
+	var txtEmail=document.getElementById("txtEmail").value;
+	var txtUser=document.getElementById("txtUser").value;
+	var txtPassword=document.getElementById("txtPassword").value;
 
+	var appendError="";
+	if(txtEmail==""){
+		appendError="email";
+	}
+	if(txtUser==""){
+		if(appendError!=""){
+			appendError=appendError+"and";
+		}
+		appendError=appendError+"un";
+	}
+	if(txtPassword==""){
+		if(appendError!=""){
+			appendError=appendError+"and";
+		}
+		appendError=appendError+"Password";
+	
+}
+if(appendError!=""){
+	appendError=appendError+"can not be empty.";
+	alert(appendError);
+	return false;
+}
+else{
+return true;
+}
+}
